f34fdb776280353cd82bf6020d89e7d2066f43b1
Aliaksander <45327050+mobilebrainz@users.noreply.github.com>
2022-04-19 12:18:56 +0300

    1

---
+++ app/src/main/java/app/khodko/planner/App.kt
@@ -0,0 +1,16 @@
+package app.khodko.planner
+
+import android.app.Application
+
+class App : Application() {
+
+    companion object {
+        lateinit var instance: App
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+}
