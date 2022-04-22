a99b384137767d635996a9385acdaca393a1be51
Aliaksander <mobilebrainz@gmail.com>
2022-04-22 20:40:38 +0300

    + notes images

--- app/src/main/java/app/khodko/planner/ui/notes/NoteViewHolder.kt
+++ app/src/main/java/app/khodko/planner/ui/notes/NoteViewHolder.kt
@@ -3,19 +3,28 @@ package app.khodko.planner.ui.notes
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import app.khodko.planner.R
+import app.khodko.planner.core.stringToBitmap
 import app.khodko.planner.data.entity.Note
 
 class NoteViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
+    private val imageView: ImageView = view.findViewById(R.id.noteImage)
     private val tittleView: TextView = view.findViewById(R.id.tittleView)
     private val textView: TextView = view.findViewById(R.id.textView)
 
     fun bind(note: Note) {
         tittleView.text = note.tittle
         textView.text = note.text
+
+        if (note.icon.isNotEmpty()) {
+            imageView.setImageBitmap(stringToBitmap(note.icon))
+        } else {
+            imageView.setImageResource(R.drawable.ic_image_outline_24)
+        }
     }
 
     companion object {
