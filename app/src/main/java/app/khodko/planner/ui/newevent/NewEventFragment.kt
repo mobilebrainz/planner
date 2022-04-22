5786c009dcfe99cbbacce749d15370598d426d86
Aliaksander <45327050+mobilebrainz@users.noreply.github.com>
2022-04-22 14:33:18 +0300

    + date picker

--- app/src/main/java/app/khodko/planner/ui/newevent/NewEventFragment.kt
+++ app/src/main/java/app/khodko/planner/ui/newevent/NewEventFragment.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import app.khodko.planner.App
 import app.khodko.planner.R
 import app.khodko.planner.core.BaseFragment
 import app.khodko.planner.core.extension.getViewModelExt
-import app.khodko.planner.data.entity.Note
+import app.khodko.planner.data.entity.Event
 import app.khodko.planner.databinding.FragmentNewEventBinding
 import app.khodko.planner.ui.addnote.AddNoteFragmentArgs
-import java.text.SimpleDateFormat
 import java.util.*
 
 class NewEventFragment : BaseFragment() {
@@ -21,6 +21,10 @@ class NewEventFragment : BaseFragment() {
 
     private lateinit var newEventViewModel: NewEventViewModel
     private var userId: Long = -1
+    private var start = false
+    private var ending = false
+    private var startDate: Date = Calendar.getInstance().time
+    private var endingDate: Date = Calendar.getInstance().time
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentNewEventBinding.inflate(inflater, container, false)
@@ -34,7 +38,9 @@ class NewEventFragment : BaseFragment() {
                 )
             }
             initObservers()
+            initListeners()
         }
+        binding.timePicker.setIs24HourView(true)
         return binding.root
     }
 
@@ -46,6 +52,84 @@ class NewEventFragment : BaseFragment() {
         }
     }
 
+    private fun initListeners() {
+        binding.startBtn.setOnClickListener {
+            binding.datePickerView.isVisible = true
+            fab.hide()
+            start = true
+            ending = false
+
+            val calendar = Calendar.getInstance()
+            calendar.time = startDate
+            binding.datePicker.updateDate(
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            )
+        }
+
+        binding.endingBtn.setOnClickListener {
+            binding.datePickerView.isVisible = true
+            fab.hide()
+            ending = true
+            start = false
+
+            val calendar = Calendar.getInstance()
+            calendar.time = endingDate
+            binding.datePicker.updateDate(
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            )
+        }
+
+        binding.datePickerCancel.setOnClickListener {
+            binding.datePickerView.isVisible = false
+            fab.show()
+            start = false
+            ending = false
+        }
+        binding.datePickerOK.setOnClickListener {
+            binding.datePickerView.isVisible = false
+            binding.timePickerView.isVisible = true
+            val calendar = Calendar.getInstance()
+            if (start) {
+                calendar.time = startDate
+                binding.timePicker.hour = calendar.get(Calendar.HOUR_OF_DAY)
+                binding.timePicker.minute = calendar.get(Calendar.MINUTE)
+            }
+            if (ending) {
+                calendar.time = endingDate
+                binding.timePicker.hour = calendar.get(Calendar.HOUR_OF_DAY)
+                binding.timePicker.minute = calendar.get(Calendar.MINUTE)
+            }
+        }
+        binding.timePickerCancel.setOnClickListener {
+            binding.timePickerView.isVisible = false
+            fab.show()
+            start = false
+            ending = false
+        }
+        binding.timePickerOK.setOnClickListener {
+            binding.timePickerView.isVisible = false
+            fab.show()
+
+            val cal = Calendar.getInstance()
+            cal.set(
+                binding.datePicker.year,
+                binding.datePicker.month,
+                binding.datePicker.dayOfMonth,
+                binding.timePicker.hour,
+                binding.timePicker.minute
+            )
+            val date = cal.time
+            if (start) startDate = date
+            if (ending) endingDate = date
+            start = false
+            ending = false
+        }
+    }
+
     private fun addEvent() {
         val tittle = binding.editTittle.text.toString().trim()
         when {
@@ -54,18 +138,15 @@ class NewEventFragment : BaseFragment() {
                 binding.editTittle.error = getString(R.string.tittle_field_error)
             }
             else -> {
-                val allDays = binding.allDaySwitch.isChecked
-                /*
-                val sdf = SimpleDateFormat("dd.MM.yyyy hh:mm:ss", Locale.US)
-                val currentDate = sdf.format(Calendar.getInstance().time)
-                val note = Note(
+                val event = Event(
                     userId = userId,
                     tittle = tittle,
-                    text = text,
-                    datetime = currentDate
+                    start = startDate.time,
+                    ending = endingDate.time,
+                    repeat = 1,
+                    allDay = binding.allDaySwitch.isChecked
                 )
-                newEventViewModel.save(note)
-                 */
+                newEventViewModel.save(event)
             }
         }
     }
@@ -77,6 +158,8 @@ class NewEventFragment : BaseFragment() {
         newEventViewModel.event.observe(viewLifecycleOwner) { e ->
             binding.editTittle.setText(e.tittle)
             binding.allDaySwitch.isChecked = e.allDay
+            startDate = Date(e.start)
+            endingDate = Date(e.ending)
         }
     }
 
